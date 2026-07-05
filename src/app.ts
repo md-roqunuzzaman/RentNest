61d6d96912a0f84ac352ae7f3cc2e3312991dd47
md-roqunuzzaman <khanrokon571@gmail.com>
2026-07-06 00:14:31 +0800

    project setup completed

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app: Application = express();
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello home");
+});
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from "./app";
+import config from "./config";
+
+const main = () => {
+  try {
+    app.listen(config.port, () => {
+      console.log(`server running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+main();
