09be4a59b20d228a9436ae0ba8259f5ef1a9c83f
md-roqunuzzaman <khanrokon571@gmail.com>
2026-07-08 15:58:33 +0800

    review controller complete

---
+++ src/modules/review/review.interface.ts
@@ -0,0 +1,5 @@
+export interface ICreateReview {
+  rentalRequestId: string;
+  rating: number;
+  comment: string;
+}
---
+++ src/modules/review/review.service.ts
@@ -0,0 +1,6 @@
+import { ICreateReview } from "./review.interface";
+
+const createReview = async (userId: string, payload: ICreateReview) => {};
+export const reviewService = {
+  createReview,
+};
