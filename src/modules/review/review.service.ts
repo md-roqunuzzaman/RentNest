5e5890dfa7098d91cd5a6a0594aff37dfd895f81
md-roqunuzzaman <khanrokon571@gmail.com>
2026-07-08 16:13:53 +0800

    review service completed with input validation

--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,6 +1,76 @@
+import { RentalStatus } from "../../../generated/prisma/enums";
+import { prisma } from "../../lib/prisma";
 import { ICreateReview } from "./review.interface";
 
-const createReview = async (userId: string, payload: ICreateReview) => {};
+const createReview = async (userId: string, payload: ICreateReview) => {
+  // rating validation
+  if (!payload.rating || payload.rating < 1 || payload.rating > 5) {
+    throw new Error("Rating must be between 1 and 5");
+  }
+
+  if (!payload.comment || payload.comment.trim().length === 0) {
+    throw new Error("Comment is required");
+  }
+
+  // check rental request
+  const rentalRequest = await prisma.rentalRequest.findUnique({
+    where: {
+      id: payload.rentalRequestId,
+    },
+  });
+
+  if (!rentalRequest) {
+    throw new Error("Rental request not found");
+  }
+
+  // only owner tenant allow for  review
+  if (rentalRequest.tenantId !== userId) {
+    throw new Error("You are not authorized to review this property");
+  }
+
+  // only after payment completed tenant can review
+  if (rentalRequest.status !== RentalStatus.ACTIVE) {
+    throw new Error("You can review only after completing rental payment");
+  }
+
+  // prevent duplicate review ,only review once
+  const existingReview = await prisma.review.findUnique({
+    where: {
+      rentalRequestId: payload.rentalRequestId,
+    },
+  });
+
+  if (existingReview) {
+    throw new Error("You already reviewed this property");
+  }
+
+  const review = await prisma.review.create({
+    data: {
+      tenantId: userId,
+      propertyId: rentalRequest.propertyId,
+      rentalRequestId: payload.rentalRequestId,
+      rating: payload.rating,
+      comment: payload.comment,
+    },
+    include: {
+      tenant: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+      property: {
+        include: {
+          category: true,
+        },
+      },
+    },
+  });
+
+  return review;
+};
+
 export const reviewService = {
   createReview,
 };
