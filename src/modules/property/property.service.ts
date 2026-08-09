349ee94c9d65e91ebed1ec306df0e5f68f8387c4
md-roqunuzzaman <khanrokon571@gmail.com>
2026-08-09 21:18:56 +0800

    fix: some issue like pegination

--- src/modules/property/property.service.ts
+++ src/modules/property/property.service.ts
@@ -16,7 +16,7 @@ interface IPropertyQuery {
 }
 
 const getAllProperties = async (query: IPropertyQuery) => {
-  const limit = query.limit ? Number(query.limit) : 10;
+  const limit = query.limit ? Number(query.limit) : 12;
   const page = query.page ? Number(query.page) : 1;
   const skip = (page - 1) * limit;
   const sortBy =
@@ -216,7 +216,7 @@ const getRelatedProperties = async (propertyId: string) => {
       ],
     },
 
-    take: 4,
+    take: 6,
 
     include: {
       category: true,
